1f471cd043e8a1ae925b4f6327643fdb6b164229
Philip Kershaw <philip.kershaw@stfc.ac.uk>
2018-08-14 11:01:39 +0100

    Added test for faceted search query.  Added ability to set service URIs using environment variables

--- ceda/cci_odp_ops_tests/test_csw.py
+++ ceda/cci_odp_ops_tests/test_csw.py
@@ -8,16 +8,27 @@ __license__ = """BSD - See LICENSE file in top-level directory"""
 __contact__ = "Philip.Kershaw@stfc.ac.uk"
 __revision__ = '$Id$'
 import unittest
+import os
 import xml.etree.ElementTree as ET
+import logging
 
 import requests
+
+log = logging.getLogger(__name__)
 
 
 class CSWTestCase(unittest.TestCase):
     '''Unit test case for testing ESA CCI Open Data Portal CSW'''
 
-    CSW_URI = 'https://csw.ceda.ac.uk/geonetwork/srv/eng/csw-CEDA-CCI'
-    DASHBOARD_CSW_QUERY_HDR = {"Content-type": "application/xml"}
+    CSW_URI = os.getenv(
+                    'CCI_CSW_TESTCASE_CSW_URI',
+                    'https://csw.ceda.ac.uk/geonetwork/srv/eng/csw-CEDA-CCI')
+    
+    VOCAB_SERVER_URI = os.getenv('CCI_CSW_TESTCASE_VOCAB_SERVER_URI', 
+                                 'http://vocab-test.ceda.ac.uk')
+    
+    CSW_QUERY_HDR = {"Content-type": "application/xml"}
+    
     DASHBOARD_CSW_QUERY_BODY = '''<?xml version="1.0" encoding="UTF-8"?>
 <csw:GetRecords
     xmlns:csw="http://www.opengis.net/cat/csw/2.0.2"
@@ -37,17 +48,58 @@ class CSWTestCase(unittest.TestCase):
     <csw:Query typeNames="csw:Record">
         <csw:ElementSetName>full</csw:ElementSetName>
         <csw:Constraint version="1.1.0">
-        <ogc:Filter>
-             <ogc:PropertyIsEqualTo>
-                 <ogc:PropertyName>AnyText</ogc:PropertyName>
-                 <ogc:Literal>%*%</ogc:Literal>
-             </ogc:PropertyIsEqualTo>
-        </ogc:Filter>
+            <ogc:Filter>
+                 <ogc:PropertyIsEqualTo>
+                     <ogc:PropertyName>AnyText</ogc:PropertyName>
+                     <ogc:Literal>%*%</ogc:Literal>
+                 </ogc:PropertyIsEqualTo>
+            </ogc:Filter>
         </csw:Constraint>
     </csw:Query>
 </csw:GetRecords>
 '''
+    # Search for all daily frequency Level 4 datasets
+    L4_DAILY_FACETED_SEARCH_QUERY_BODY = '''<?xml version="1.0" encoding="UTF-8"?>
+<csw:GetRecords
+    xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" 
+    xmlns:ogc="http://www.opengis.net/ogc" 
+    xmlns:gml="http://www.opengis.net/gml/3.2" 
+    xmlns:dc="http://purl.org/dc/elements/1.1/" 
+    xmlns:dct="http://purl.org/dc/terms/" 
+    xmlns:gmd="http://www.isotc211.org/2005/gmd" 
+    xmlns:gco="http://www.isotc211.org/2005/gco" 
+    xmlns:geonet="http://www.fao.org/geonetwork" 
+    service="CSW" 
+    version="2.0.2" 
+    resultType="results_with_summary" 
+    outputSchema="http://www.isotc211.org/2005/gmd" 
+    startPosition="1"
+    maxRecords="1">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0">
+            <ogc:Filter>
+                <ogc:And>
+                    <ogc:PropertyIsEqualTo>
+                        <ogc:PropertyName>AnyText</ogc:PropertyName>
+                        <ogc:Literal>%*%</ogc:Literal>
+                    </ogc:PropertyIsEqualTo>
+                    <ogc:PropertyIsEqualTo>
+                        <ogc:PropertyName>keywordUri</ogc:PropertyName>
+                        <ogc:Literal>{}/collection/cci/procLev/proc_level4</ogc:Literal>
+                    </ogc:PropertyIsEqualTo>
+                    <ogc:PropertyIsEqualTo>
+                        <ogc:PropertyName>keywordUri</ogc:PropertyName>
+                        <ogc:Literal>{}/collection/cci/freq/freq_day</ogc:Literal>
+                    </ogc:PropertyIsEqualTo>
+                </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>
+'''.format(*2*(VOCAB_SERVER_URI,))
 
+    CSW_NS_URI = "http://www.opengis.net/cat/csw/2.0.2"
     GMD_NS_URI = "http://www.isotc211.org/2005/gmd"
     GML_NS_URI = "http://www.opengis.net/gml/3.2"
     GCO_NS_URI = "http://www.isotc211.org/2005/gco"
@@ -58,15 +110,24 @@ class CSWTestCase(unittest.TestCase):
     def _csw_query(self, csw_uri, post_query, headers):
         response = requests.post(csw_uri, data=post_query, headers=headers)
 
-        resp_elem = ET.fromstring(response.text)
+        if response.text:
+            resp_elem = ET.fromstring(response.text)
+            log.debug(response.text)
+        else:
+            resp_elem = None
 
         return response.status_code, resp_elem
 
     def test01_csw_dashboard_query(self):
         status_code, resp_elem = self._csw_query(self.__class__.CSW_URI,
                                      self.__class__.DASHBOARD_CSW_QUERY_BODY,
-                                     self.__class__.DASHBOARD_CSW_QUERY_HDR)
-        self.assertEqual(status_code, 200, msg="Expecting 200 OK response code")
+                                     self.__class__.CSW_QUERY_HDR)
+        
+        self.assertEqual(status_code, 200, 
+                         msg="Expecting 200 OK response code")
+  
+        self.assertIsNotNone(resp_elem, 'Got empty response')
+      
         self.assertEqual(resp_elem.tag, self.__class__.GET_RECS_RESP_TAG,
                          msg="Expecting {} tag in response for {!r}".format(
                                self.__class__.GET_RECS_RESP_TAG,
@@ -75,10 +136,12 @@ class CSWTestCase(unittest.TestCase):
     def test02_csw_dashboard_check_temporal_extent(self):
         status_code, resp_elem = self._csw_query(self.__class__.CSW_URI,
                                      self.__class__.DASHBOARD_CSW_QUERY_BODY,
-                                     self.__class__.DASHBOARD_CSW_QUERY_HDR)
+                                     self.__class__.CSW_QUERY_HDR)
         self.assertEqual(status_code, 200,
                          msg="Expecting 200 OK response code for {!r}".format(
                              self.__class__.CSW_URI))
+
+        self.assertIsNotNone(resp_elem, 'Got empty response')
 
         temporal_start_time = resp_elem.findall(
             ".//{%s}TimePeriod/{%s}beginPosition"
@@ -100,10 +163,12 @@ class CSWTestCase(unittest.TestCase):
     def test03_csw_dashboard_check_geographic_extent(self):
         status_code, resp_elem = self._csw_query(self.__class__.CSW_URI,
                                      self.__class__.DASHBOARD_CSW_QUERY_BODY,
-                                     self.__class__.DASHBOARD_CSW_QUERY_HDR)
+                                     self.__class__.CSW_QUERY_HDR)
         self.assertEqual(status_code, 200,
                          msg="Expecting 200 OK response code for {!r}".format(
                              self.__class__.CSW_URI))
+
+        self.assertIsNotNone(resp_elem, 'Got empty response')
 
         west_bound_lon = resp_elem.findall(
             ".//{%s}geographicElement"
@@ -126,4 +191,28 @@ class CSWTestCase(unittest.TestCase):
         self.assertGreater(len(south_bound_lat), 0,
                         msg="No South Bound Latitude elements found in "
                         "response {!r}".format(self.__class__.CSW_URI))
-
+        
+    def test04_csw_l4_daily_faceted_search_query(self):
+        status_code, resp_elem = self._csw_query(self.__class__.CSW_URI,
+                         self.__class__.L4_DAILY_FACETED_SEARCH_QUERY_BODY,
+                         self.__class__.CSW_QUERY_HDR)
+        
+        self.assertEqual(status_code, 200,
+                         msg="Expecting 200 OK response code for {!r}".format(
+                             self.__class__.CSW_URI))
+        
+        self.assertIsNotNone(resp_elem, 'Got empty response')
+        
+        self.assertEqual(resp_elem.tag, self.__class__.GET_RECS_RESP_TAG,
+                         msg="Expecting {} tag in response for {!r}".format(
+                               self.__class__.GET_RECS_RESP_TAG,
+                               self.__class__.CSW_URI))
+        
+        search_results = resp_elem.find(".//{%s}SearchResults" % 
+                                        self.__class__.CSW_NS_URI)
+        
+        n_search_results_matched = int(search_results.attrib.get(
+                                                'numberOfRecordsMatched', 0))
+            
+        self.assertGreater(n_search_results_matched, 0, 
+                           msg='No search results matched')
